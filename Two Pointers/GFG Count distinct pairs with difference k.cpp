53c7d7ad91d6148b138d1937e1769d4169f4b70f
Nitish Kumar <nitish120800@gmail.com>
2022-07-09 01:31:20 +0530

    Update GFG Count distinct pairs with difference k.cpp

--- Two Pointers/GFG Count distinct pairs with difference k.cpp
+++ Two Pointers/GFG Count distinct pairs with difference k.cpp
@@ -3,63 +3,63 @@
 class Solution{
 	public:
 	int TotalPairs(vector<int>arr, int k){
-	    // Code here
-	    int n=arr.size();
-	    unordered_map<int,int>m;
-        int count=0;
-        int i=0;
-        for(int j=0;j<n;J++){
-            m[arr[j]]++;
-        }
-        for(;i<n;){
-            int b,c;
-            if(arr[i]<k){
-                b=k+arr[i];
-                int a=arr[i];
-                while(arr[i]==a){
-                    i++;
-                }
-            }
-            else if(arr[i]>k){
-                // we have to consider both the case where the both the elements are greater than target 
-                // one is larger and other is smaller
-                // both are smaller tyhan target
-                b=arr[i]-k;
-                if(k>0){c=k+arr[i];}
-                int a=arr[i];
-                while(arr[i]==a){
-                    i++;
-                }
-            }
-            if(m[b]){
-                count++;
-            }
-            else if(m[c]){
-                count++;
-            }
-            
-        }
-        return count;
-        // unordered_map<int,int>mp;
+	    // // Code here
+	    // int n=arr.size();
+	    // unordered_map<int,int>m;
         // int count=0;
-        // for(int i=0;i<arr.size();i++)
-        // {
-        //     mp[arr[i]]++;
+        // int i=0;
+        // for(int j=0;j<n;J++){
+        //     m[arr[j]]++;
         // }
-        // for(auto x:mp)
-        // {
-        //     if(mp.find(x.first-k)!=mp.end())
-        //     {
-        //         if((x.first-k)==x.first)
-        //         {
-        //             if(x.second>1)
-        //             count++;
+        // for(;i<n;){
+        //     int b,c;
+        //     if(arr[i]<k){
+        //         b=k+arr[i];
+        //         int a=arr[i];
+        //         while(arr[i]==a){
+        //             i++;
         //         }
-        //         else
+        //     }
+        //     else if(arr[i]>k){
+        //         // we have to consider both the case where the both the elements are greater than target 
+        //         // one is larger and other is smaller
+        //         // both are smaller tyhan target
+        //         b=arr[i]-k;
+        //         if(k>0){c=k+arr[i];}
+        //         int a=arr[i];
+        //         while(arr[i]==a){
+        //             i++;
+        //         }
+        //     }
+        //     if(m[b]){
         //         count++;
         //     }
+        //     else if(m[c]){
+        //         count++;
+        //     }
+            
         // }
         // return count;
+        unordered_map<int,int>mp;
+        int count=0;
+        for(int i=0;i<arr.size();i++)
+        {
+            mp[arr[i]]++;
+        }
+        for(auto x:mp)
+        {
+            if(mp.find(x.first-k)!=mp.end())
+            {
+                if((x.first-k)==x.first)
+                {
+                    if(x.second>1)
+                    count++;
+                }
+                else
+                count++;
+            }
+        }
+        return count;
     	}
 
 };
