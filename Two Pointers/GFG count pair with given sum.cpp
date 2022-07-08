2c4037ff97b18516c52c75bcf0c3e7db39c3e4c3
Nitish Kumar <nitish120800@gmail.com>
2022-07-09 00:23:32 +0530

    Update GFG count pair with given sum.cpp

--- Two Pointers/GFG count pair with given sum.cpp
+++ Two Pointers/GFG count pair with given sum.cpp
@@ -2,46 +2,16 @@
 class Solution{   
 public:
     int getPairsCount(int arr[], int n, int x) {
-        // code here
-        map<int,int>m;
-        int i=0;
         int count=0;
-        for(;i<n;){
-            if(arr[i]==(x-arr[i])){
-                int c=0;
-                int val1=arr[i];
-                while(i<n && arr[i]==val1){
-                    i++;
-                    c++;
-                }
-                count+=(c*(c-1))/2;
-                // cout<<"alone"<<count<<endl;
+        //create unordered_map so that the inserting will take O(1)
+        unordered_map<int, int>m;
+        for(int i=0; i<n;i++){
+            int b=x-arr[i];
+            // if x-arr[i] exist then unc count and then inc the occurence of arr[i]
+            if(m[b]){
+                count+=m[b];
             }
-            else if(m.find(arr[i])!=m.end()){
-                int c1=0;
-                int j=i;
-                int val=arr[i];
-                while(i<n && arr[i]==val){
-                    c1++;
-                    i++;
-                }
-                m[(x-arr[j])]=c1;
-                count+=c1*m[arr[j]];
-                // cout<<"adding"<<count<<endl;
-            }
-            else{
-                int c2=0;
-                int j=i;
-                int val2=arr[i];
-                while(i<n && arr[i]==val2){
-                    c2++;
-                    // cout<<"c2"<<c2<<endl;
-                    i++;
-                }
-                m[(x-arr[j])]=c2;
-                // cout<<"insert"<<count<<m[5]<<endl;
-            }
-            
+            m[arr[i]]++;
         }
         return count;
     }
