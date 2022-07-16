1bbad369e8cd2e75528b6ed5c33bccd97e23086e
Nitish <nitish120800@gmail.com>
2022-07-16 15:17:40 +0530

    Update GFG Key Pair.cpp

--- Hash Map/GFG Key Pair.cpp
+++ Hash Map/GFG Key Pair.cpp
@@ -1,54 +1,56 @@
-// Given an array of positive and negative numbers. Find if there is a subarray (of size at-least one) with 0 sum.
+// Given an array Arr of N positive integers and another number X. Determine whether or not there exist two elements in Arr whose sum is exactly X.
 //{ Driver Code Starts
-// A C++ program to find if there is a zero sum
-// subarray
 #include <bits/stdc++.h>
+
 using namespace std;
 
 
-
 // } Driver Code Ends
+//User function template for C++
 class Solution{
-    public:
-    //Complete this function
-    //Function to check whether there is a subarray present with 0-sum or not.
-    bool subArrayExists(int arr[], int n)
-    {
-        //Your code here
-        unordered_map<int,int>m;
-        int sum=0;
-        m[sum]++;
-        for(int i=0;i<n;i++){
-            sum+=arr[i];
-            if(m.find(sum)!=m.end()){
-                return true;
-                break;
-            }
-            m[sum]++;
-        }
-        return false;
-    }
+public:	
+	// Function to check if array has 2 elements
+	// whose sum is equal to the given value
+	bool hasArrayTwoCandidates(int arr[], int n, int x) {
+	    // code here
+	    unordered_map<int,int>m;
+	    for(int i=0;i<n;i++){
+	        m[arr[i]]++;
+	        if(x-arr[i]==arr[i]){
+	            if(m.find(arr[i])!=m.end() && m[arr[i]]>1){
+	                return true;
+	                break;
+	            }
+	        }
+	        else{
+	            if(m.find(x-arr[i])!=m.end()){
+	                return true;
+	                break;
+	            }
+	        }
+	        
+	    }
+	    return false;
+	}
 };
 
 //{ Driver Code Starts.
-// Driver code
-int main()
-{
-	int t;
-	cin>>t;
-	while(t--)
-	{
-	    int n;
-	    cin>>n;
-	    int arr[n];
-	    for(int i=0;i<n;i++)
-	    cin>>arr[i];
-	    Solution obj;
-	    	if (obj.subArrayExists(arr, n))
-		cout << "Yes\n";
-	else
-		cout << "No\n";
-	}
-	return 0;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, x;
+        cin >> n >> x;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.hasArrayTwoCandidates(arr, n, x);
+        cout << (ans ? "Yes\n" : "No\n");
+    }
+    return 0;
 }
+
 // } Driver Code Ends
