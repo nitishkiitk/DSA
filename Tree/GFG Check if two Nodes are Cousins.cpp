de01501b7c22cfd571a165d0cfcf414d31b08093
Nitish <nitish120800@gmail.com>
2022-08-12 23:44:08 +0530

    Update GFG Check if two Nodes are Cousins.cpp

--- Tree/GFG Check if two Nodes are Cousins.cpp
+++ Tree/GFG Check if two Nodes are Cousins.cpp
@@ -1,27 +1,123 @@
 // Given the binary Tree of and two-node values. Check whether the two-node values are cousins of each other or not.
-bool isCousins(Node* root, int x, int y) {
-  queue<Node*> q, q1;
-  bool cousin = false;
-  bool siblings = false;
-  q.push(root);
-  while (!q.empty() && !cousin) {
-    while (!q.empty()) {
-      auto n = q.front();
-      q.pop();
-      if (n == nullptr) siblings = false;
-      else {
-        if (n->data == x || n->data == y) {
-          if (!cousin) cousin = siblings = true;
-          else return !siblings;
-        }
-        q1.push(n->left), q1.push(n->right), q1.push(nullptr);
-      }
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
     }
-    swap(q, q1);
-  }
-  return false;
+};
+// Function to Build Tree
+Node* buildTree(string str)
+{   
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+    // Creating vector of strings from input 
+    // string after spliting by space
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    // Create the root of the tree
+    Node* root = new Node(stoi(ip[0]));
+        
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+        
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+            
+        // Get the current node's value from the string
+        string currVal = ip[i];
+            
+        // If the left child is not null
+        if(currVal != "N") {
+                
+            // Create the left child for the current node
+            currNode->left = new Node(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+            
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+            
+        // If the right child is not null
+        if(currVal != "N") {
+                
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    return root;
 }
-------------------------------------------------------------------------------------------
+
+bool isCousins(Node*root , int x , int y);
+
+int main()
+{
+
+    int t;
+	scanf("%d ",&t);
+    while(t--)
+    {
+        string s;
+		getline(cin,s);
+        Node* root = buildTree(s);
+        int x , y;
+        scanf("%d ",&x);
+		scanf("%d ",&y);
+        if(x==y)
+        {
+            cout<<"0\n";
+            continue;
+        }
+        cout <<isCousins(root,x,y)<< endl;
+    }
+    return 1;
+}
+// } Driver Code Ends
+
+
+/*Complete the function below
+Node is as follows:
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+// Returns true if the nodes with values 'a' and 'b' are cousins. Else returns false
 int level(Node* root, int a, int h){
     if(!root)return 0;
     if(root->data==a) return h;
@@ -33,8 +129,10 @@ int level(Node* root, int a, int h){
     return l;
 }
 bool issibling(Node*root, int a, int b){
-    if(!root || !a || !b)return false;
-    return ((root->left->data==a && root->right->data==b)||(root->left->data==b && root->right->data==a)||issibling(root->left, a,b) || issibling(root->right,a,b));
+    if(!root)return false;
+    if(!root->left)return issibling(root->right,a,b);
+    if(!root->right)return issibling(root->left,a,b);
+    return (root->left->data==a && root->right->data==b)||(root->left->data==b && root->right->data==a)|| issibling(root->left, a,b) || issibling(root->right,a,b);
 }
 // Returns true if the nodes with values 'a' and 'b' are cousins. Else returns false
 bool isCousins(Node *root, int a, int b)
@@ -42,10 +140,10 @@ bool isCousins(Node *root, int a, int b)
    //add code here.
    if(!root || !a || !b)return false;
    int h1=1, h2=1;
-   h1=level(root, a, h1);
-   h2=level(root, b, h2);
+   int levela=level(root, a, h1);
+   int levelb=level(root, b, h2);
 //   cout<<h1 <<h2;
-   if(h1!=h2)return false;
+   if(levela!=levelb)return false;
    return !issibling(root, a,b);
    
 }
